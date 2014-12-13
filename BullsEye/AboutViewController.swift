6138aca0c2d33643a56bce5c09470f4224f8081e
Andrew Hercules <andrewhercules@gmail.com>
2014-12-13 19:25:33 +0000

    Basic app completed - About page created with segues between it and main view page

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Andrew Hercules on 2014-12-13.
+//  Copyright (c) 2014 Andrew Hercules. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+  @IBAction func close() {
+    dismissViewControllerAnimated(true, completion: nil)
+  }
+
+}
