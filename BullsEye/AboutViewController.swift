df480639225984aff7e59da0c76de86f57367c8d
Andrew Hercules <andrewhercules@gmail.com>
2014-12-15 13:21:44 -0500

    Removed text view from About page and replaced with web view and associated html file

--- BullsEye/AboutViewController.swift
+++ BullsEye/AboutViewController.swift
@@ -13,5 +13,16 @@ class AboutViewController: UIViewController {
   @IBAction func close() {
     dismissViewControllerAnimated(true, completion: nil)
   }
-
+  
+  @IBOutlet weak var webView: UIWebView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    if let htmlFile = NSBundle.mainBundle().pathForResource("BullsEye", ofType: "html") {
+      let htmlData = NSData(contentsOfFile: htmlFile)
+      let baseURL = NSURL.fileURLWithPath( NSBundle.mainBundle().bundlePath)
+      webView.loadData(htmlData, MIMEType: "text/html", textEncodingName: "UTF-8", baseURL: baseURL)
+    }
+  }
+  
 }
